ae2ef95066c0fea08232708cb0b1436e867e162a
lyntechi <lynda-santiago@lambdastudents.com>
2020-06-17 00:30:44 -0400

    changed background color

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -5,10 +5,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to
+          Edit <code>src/App.js</code> and save to reload
         </p>
         <a
           className="App-link"
@@ -18,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      
     </div>
   );
 }
